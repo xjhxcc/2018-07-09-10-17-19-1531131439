1529b7a2e0731910f6d56b2c3f6eef240bd99afd
xjhxcc <2351889342@qq.com>
2019-07-11 22:29:54 +0800

    completed add attributes and functions for person

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -1,4 +1,22 @@
 package practice01;
 
 public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    public String introduce(){
+        return "My name is "+getName()+". I am "+getAge()+" years old.";
+    }
 }
