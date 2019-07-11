4d3b4a703db9577f4bf0ca92a913dd0995f55686
xjhxcc <2351889342@qq.com>
2019-07-12 00:36:34 +0800

    completed practice07

--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,33 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce(){
+        if(klass!=null){
+            return super.introduce()+" I am a Teacher. I teach Class "+klass.getNumber()+".";
+        }else{
+            return super.introduce()+" I am a Teacher. I teach No Class.";
+        }
+    }
+    public String introduceWith(Student stu){
+        if(stu.getKlass().getNumber()==klass.getNumber()){
+            return super.introduce() + " I am a Teacher. I teach "+stu.getName()+".";
+        }else{
+            return super.introduce()+" I am a Teacher. I don't teach "+stu.getName()+".";
+        }
+    }
 }
+
