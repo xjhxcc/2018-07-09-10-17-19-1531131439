0356b97ca452583dd78a500ab0cd1689e9facfe0
xjhxcc <2351889342@qq.com>
2019-07-12 00:30:22 +0800

    completed practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,25 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Integer klass;
+
+    public Teacher(String name, int age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    public String introduce(){
+        if(klass!=null){
+            return super.introduce()+" I am a Teacher. I teach Class "+klass+".";
+        }else{
+            return super.introduce()+" I am a Teacher. I teach No Class.";
+        }
+    }
 }
